df7fec8475913beeb2114b4d3f7896c4df2c9553
Anurag Pandey <64393814+anuragpandey1115@users.noreply.github.com>
2021-03-13 01:21:42 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import '../App.css';
 import Convertor from './Convertor';
-// import CurrencyRow from '../CurrencyRow';
+
 
 export class home extends Component {
     render() {
