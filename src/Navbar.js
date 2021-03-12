46f0c9137c25261dd54e378265f4ab52e01127d3
Anurag Pandey <64393814+anuragpandey1115@users.noreply.github.com>
2021-03-13 01:11:58 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -28,20 +28,7 @@ export class Navbar extends Component {
                   <li className="nav-item">
                     <Link className="nav-link active" to="Convertor">Convertor</Link>
                   </li>
-                  {/* <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                 </a>
-                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li><hr className="dropdown-divider" /></li>
-                      <li><a className="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li> */}
-                  {/* <li className="nav-item">
-                    <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                  </li> */}
+                 
                 </ul>
 
               </div>
